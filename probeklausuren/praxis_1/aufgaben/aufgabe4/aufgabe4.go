e672f3718f8b92f70443e90e90679b4c7271ee23
BenLuisRichter <s252793@student.dhbw-mannheim.de>
2026-06-12 15:31:07 +0000

    Lösung der Aufgabe und Klausurrelevante Themen

--- probeklausuren/praxis_1/aufgaben/aufgabe4/aufgabe4.go
+++ probeklausuren/praxis_1/aufgaben/aufgabe4/aufgabe4.go
@@ -1,5 +1,7 @@
 package aufgabe4
 
+import "slices"
+
 // AUFGABENSTELLUNG: Vervollständigen Sie die vorgegebene Funktion.
 // KONTEXT: In der Datei graph.go finden Sie eine Implementierung eines Knotens in einem
 //          Graphen. Die zu implementierende Funktion ist eine Methode dieses Structs.
@@ -7,8 +9,53 @@ package aufgabe4
 
 // ReachableNodes soll eine Liste aller von n aus erreichbaren Knoten liefern.
 func (n *Node) ReachableNodes() []*Node {
-	visitedNodes := []*Node{}
+	alreadyVisited := []*Node{} //um Duplikate zu vermeiden, speichern wir hier alle bereits besuchten Knoten
+	toVisit := []*Node{n}
 
-	// TODO
+	//Grundsätzlicher Ansatz einer Breitensuche
+	//Eine Liste von Knoten pflegen, die noch abzuarbeiten sind
+	//Die Suche endet, wenn diese Liste leer ist
+	for len(toVisit) > 0 {
+		first := toVisit[0]
+		toVisit = toVisit[1:]
+		if !slices.Contains(alreadyVisited, first) {
+			alreadyVisited = append(alreadyVisited, first)
+			toVisit = append(toVisit, first.neighbours...)
+		}
+	}
+
+	return alreadyVisited
+}
+
+//Klausurrelevante Themen
+//Algorithmen (Dijkstra, Floyd, A*)
+//ob einer von den algorithmen sinnvoll für eine Aufgabe ist
+//Hashmaps Funktionsweise und Komplexität
+
+/* Tiefensuche, rekursiv
+func (n *Node) ReachableNodes2() []*Node {
+	visitedNodes := []*Node{}
+	result := []*Node{n}
+	x := 0
+
+	for len(visitedNodes) != len(result) {
+		for _, node := range n.ReachableIn(x) {
+			//...
 	return visitedNodes
 }
+
+func (n *Node) ReachableNodesIn(x int) []*Node {
+	visitedNodes := []*Node{n}
+	if x == 0 {
+		return visitedNodes
+	}
+
+	rfn := []*Node{}
+	for _, nb := range n.neighbours {
+		rfn = append(rfn, nb.ReachableNodesIn(x-1)...)
+	}
+	rfn = append(rfn, n)
+
+	return visitedNodes
+}
+*/
